ab110d31f96508e492dd5d9c350e9a59b1e475c6
Harsh2302 <harshdeepgarg620@gmail.com>
2025-12-28 17:42:05 +1100

    Landing screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div>
-      <Button>hi</Button>
+      <Header/>
+      <Hero/>
     </div>
   );
 }
